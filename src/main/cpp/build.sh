7a156633380e70eb9a93c38d788c0bcf4d7ff678
Hongze Zhang <hongze.zzz123@gmail.com>
2025-02-19 21:59:09 +0800

    Throw a meaningful error message when the current platform is not supported by the Jar (#38)

--- src/main/cpp/build.sh
+++ src/main/cpp/build.sh
@@ -4,18 +4,19 @@ set -e
 set -o pipefail
 set -u
 
-BASE_DIR=$(dirname $0)
+BASE_DIR=$(dirname "$0")
 NUM_THREADS=$(nproc)
 SOURCE_DIR=$BASE_DIR
 BUILD_DIR=$BASE_DIR/build
-INSTALL_DIR=$BUILD_DIR/dist
-INSTALL_LIB_DIR=$INSTALL_DIR/lib
+INSTALL_PREFIX=$BUILD_DIR/dist
+INSTALL_DESTINATION=lib
+INSTALL_LIB_DIR=$INSTALL_PREFIX/$INSTALL_DESTINATION
 VELOX4J_LIB_NAME=libvelox4j.so
 
 # Build C++ so libraries.
-cmake -DCMAKE_BUILD_TYPE=Release -DVELOX4J_BUILD_TESTING=OFF -S "$SOURCE_DIR" -B "$BUILD_DIR"
+cmake -DCMAKE_BUILD_TYPE=Release -DVELOX4J_BUILD_TESTING=OFF -DVELOX4J_INSTALL_DESTINATION="$INSTALL_DESTINATION" -S "$SOURCE_DIR" -B "$BUILD_DIR"
 cmake --build "$BUILD_DIR" --target velox4j-shared -j "$NUM_THREADS"
-cmake --install "$BUILD_DIR" --component velox4j --prefix "$INSTALL_DIR"
+cmake --install "$BUILD_DIR" --component velox4j --prefix "$INSTALL_PREFIX"
 
 # Resolve symlinks in the library installation directory.
 for file in "$INSTALL_LIB_DIR"/*
--- src/main/java/io/github/zhztheplayer/velox4j/jni/JniLibLoader.java
+++ src/main/java/io/github/zhztheplayer/velox4j/jni/JniLibLoader.java
@@ -1,6 +1,7 @@
 package io.github.zhztheplayer.velox4j.jni;
 
 import com.google.common.base.Preconditions;
+import com.google.common.base.StandardSystemProperty;
 import io.github.zhztheplayer.velox4j.exception.VeloxException;
 import io.github.zhztheplayer.velox4j.resource.ResourceFile;
 import io.github.zhztheplayer.velox4j.resource.Resources;
@@ -14,7 +15,7 @@ import java.util.stream.Collectors;
 public class JniLibLoader {
   private static final AtomicBoolean LOADED = new AtomicBoolean(false);
 
-  private static final String LIB_CONTAINER = "velox4j-lib";
+  private static final String LIB_CONTAINER = String.format("velox4j-lib/%s/%s", StandardSystemProperty.OS_NAME.value(), StandardSystemProperty.OS_ARCH.value());
   private static final Pattern LIB_PATTERN = Pattern.compile("^.+$");
   private static final String VELOX4J_LIB_NAME = "libvelox4j.so";
 
@@ -24,16 +25,21 @@ public class JniLibLoader {
     }
     Preconditions.checkArgument(workDir.isDirectory(), "Work directory %s is not a directory", workDir);
     final List<ResourceFile> libFiles = Resources.getResources(LIB_CONTAINER, LIB_PATTERN);
+    if (libFiles.isEmpty()) {
+      throw new VeloxException(String.format("Library container %s not found in classpath. Please check whether the current platform is supported by the Jar", LIB_CONTAINER));
+    }
     final List<ResourceFile> velox4jLibFiles = libFiles.stream().filter(f -> f.name().equals(VELOX4J_LIB_NAME)).collect(Collectors.toList());
     Preconditions.checkArgument(velox4jLibFiles.size() == 1, "Velox4j library not found");
+    System.out.printf("Found required libraries in container %s.%n", LIB_CONTAINER);
     final ResourceFile velox4jLibFile = velox4jLibFiles.get(0);
     for (ResourceFile libFile : libFiles) {
       final File copied = workDir.toPath().resolve(libFile.name()).toFile();
-      System.out.printf("Copying library %s to %s...%n", libFile.name(), copied);
+      System.out.printf("Copying library %s/%s to %s...%n", LIB_CONTAINER, libFile.name(), copied);
       libFile.copyTo(copied);
     }
     final File copiedVelox4jLib = workDir.toPath().resolve(velox4jLibFile.name()).toFile();
     Preconditions.checkState(copiedVelox4jLib.isFile(), "Velox4j library not copied to work directory");
     System.load(copiedVelox4jLib.getAbsolutePath());
+    System.out.printf("All required libraries were successfully loaded.%n", LIB_CONTAINER);
   }
 }
