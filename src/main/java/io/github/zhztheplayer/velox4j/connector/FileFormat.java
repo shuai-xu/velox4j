b0a99a7291ad649f1176a9b9a1f87c7fb24255d3
Hongze Zhang <hongze.zzz123@gmail.com>
2025-02-19 21:22:03 +0800

    Update organization name (#37)

--- src/main/java/io/github/zhztheplayer/velox4j/connector/FileFormat.java
+++ src/main/java/io/github/zhztheplayer/velox4j/connector/FileFormat.java
@@ -1,6 +1,5 @@
 package io.github.zhztheplayer.velox4j.connector;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum FileFormat {
